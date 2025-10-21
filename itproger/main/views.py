d4d0bfc961569baa12a2b10d5a30c6c9bf078849
l.romadin (Air) <zemferonok@gmail.com>
2025-10-21 16:08:39 +0200

    lesson_6

--- itproger/main/views.py
+++ itproger/main/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request ):
-    return render(request, 'main/index.html')
-# templates/main/index.html
 
-def about(request ):
-    return render(request, 'main/about.html')
+def index(request):
+    data = {
+        "title": "Main Page!",
+        "values": ["Word", 123, True, "Fourth"],
+    }
+    return render(
+        request, "main/index.html", data
+    )  # The actually way is "templates/main/index.html"
 
-def other(request ):
-    return HttpResponse('<h2>All other not working urls</h2>')
+
+def about(request):
+    return render(request, "main/about.html")
+
+
+def other(request):
+    return HttpResponse("<h2>All other not working urls</h2>")
