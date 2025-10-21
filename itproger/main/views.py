dd6bddae72aff1b5c76875c4dfb79b68f5186bc6
l.romadin (Air) <zemferonok@gmail.com>
2025-10-21 14:47:06 +0200

    lesson_5

--- itproger/main/views.py
+++ itproger/main/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 def index(request ):
     return render(request, 'main/index.html')
+# templates/main/index.html
 
 def about(request ):
     return render(request, 'main/about.html')
