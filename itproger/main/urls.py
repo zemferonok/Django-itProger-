4dc208abe4c12ab644efe5ab53c715ff7934e117
l.romadin (Air) <zemferonok@gmail.com>
2025-10-21 15:04:24 +0200

    lesson_5 + .gitignore

--- itproger/main/urls.py
+++ itproger/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 from . import views
  
 urlpatterns = [
-    path('', views.index),
-    path('about', views.about),
+    path('', views.index, name='home'),
+    path('about', views.about, name='about'),
     re_path(r'^.*$', views.other),
 ]
